04cf4503cf9dce8e8c137859ecf54265b4aa3faf
陈正清MacPro <tenchenzhengqing@qq.com>
2023-06-23 18:31:08 +0800

    安装了sass来管理样式 设置了无法选中文字、无法拖动图片 修改了背景颜色来测试

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -3,7 +3,7 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-06-23 16:19:13
  * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-06-23 17:54:16
+ * @LastEditTime: 2023-06-23 18:22:50
  * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/main.tsx
  * @Description: 整个项目的入口文件
  * 
@@ -15,7 +15,11 @@ import ReactDOM from 'react-dom/client'
 import 'reset-css'
 // UI框架的样式
 
+// 全局样式
+import './assets/styles/global.scss'
+
 // 组件的样式
+
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(

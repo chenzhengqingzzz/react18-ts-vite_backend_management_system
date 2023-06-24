446fc36787e5a895336b6d33873874c548f6b57a
陈正清MacPro <tenchenzhengqing@qq.com>
2023-06-24 19:46:11 +0800

    编程式导航写法

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,30 +3,26 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-06-23 16:19:13
  * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-06-24 19:05:26
+ * @LastEditTime: 2023-06-24 19:40:55
  * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/App.tsx
  * @Description: 所有组件的父组件App
  * 
  * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
  */
 import { useState } from 'react'
-import Comp1 from './components/Comp1'
-import Comp2 from './components/Comp2'
-import { Button } from 'antd'
-import { FastBackwardOutlined } from '@ant-design/icons'
-import { Outlet } from 'react-router-dom'
+// import Comp1 from './components/Comp1'
+// import Comp2 from './components/Comp2'
+// import { Button } from 'antd'
+// import { FastBackwardOutlined } from '@ant-design/icons'
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      {/* 顶级组件
-      <Comp1></Comp1>
-      <Comp2></Comp2>
-      <Button type='primary'>Primary Button</Button>
-      <FastBackwardOutlined style={{fontSize: '40px', color: '#08c'}} /> */}
-
+      <Link to='/home'>Home</Link>
+      <Link to='/about'>About</Link>
       {/* 占位符组件 类似于窗口，用来展示组件 有点像Vue中的router-view */}
       <Outlet></Outlet>
     </div>

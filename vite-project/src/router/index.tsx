efce056d36282ab53408ac8606e3c8dcebc9edd6
陈正清MacPro <tenchenzhengqing@qq.com>
2023-06-24 19:52:00 +0800

    路由重定向

--- vite-project/src/router/index.tsx
+++ vite-project/src/router/index.tsx
@@ -3,7 +3,7 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-06-24 18:18:16
  * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-06-24 18:28:00
+ * @LastEditTime: 2023-06-24 19:48:52
  * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/router/index.tsx
  * @Description: React路由
  *
@@ -12,7 +12,7 @@
 import App from "@/App";
 import Home from "@/views/Home";
 import About from "@/views/About";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // 两种路由模式的组件： BrowserRouter ( History模式 ) ， HashRouter( Hash模式 )
 
 
@@ -21,6 +21,9 @@ const router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
+          {/* 配置用户访问/的时候重定向到/home路径 */}
+          <Route path="/" element={<Navigate to="/home" />}></Route>
+
           <Route path="/home" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
         </Route>

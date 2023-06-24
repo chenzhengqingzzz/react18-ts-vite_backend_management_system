aa797721d28d2933eaf74a72409de52888fec9c7
陈正清MacPro <tenchenzhengqing@qq.com>
2023-06-24 14:46:17 +0800

    给引入的代表src的@路径增加了代码提示

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -3,7 +3,7 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-06-23 16:19:13
  * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-06-23 18:46:13
+ * @LastEditTime: 2023-06-24 14:44:41
  * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/main.tsx
  * @Description: 整个项目的入口文件
  * 

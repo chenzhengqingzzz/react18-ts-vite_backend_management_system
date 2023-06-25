e9feca3841ad7ff01f974d2a88e2a55cf8905033
陈正清macbook pro <tenchenzhengqing@qq.com>
2023-06-25 23:31:00 +0800

    调整界面样式对齐方式

--- vite-project/src/views/Home.tsx
+++ vite-project/src/views/Home.tsx
@@ -3,77 +3,96 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-06-24 18:15:11
  * @LastEditors: 陈正清macbook pro
- * @LastEditTime: 2023-06-25 18:17:08
+ * @LastEditTime: 2023-06-25 20:53:26
  * @FilePath: /react18-ts-vite_backend_management_system/vite-project/src/views/Home.tsx
  * @Description: Home首页组件
- * 
+ *
  * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
  */
 import {
-    DesktopOutlined,
-    FileOutlined,
-    PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
-  } from '@ant-design/icons';
-  import type { MenuProps } from 'antd';
-  import { Breadcrumb, Layout, Menu } from 'antd';
-  import React, { useState } from 'react';
-  
-  const { Header, Content, Footer, Sider } = Layout;
-  
-  type MenuItem = Required<MenuProps>['items'][number];
-  
-  function getItem(
-    label: React.ReactNode,
-    key: React.Key,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
-  ): MenuItem {
-    return {
-      key,
-      icon,
-      children,
-      label,
-    } as MenuItem;
-  }
-  
-  const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-      getItem('Tom', '3'),
-      getItem('Bill', '4'),
-      getItem('Alex', '5'),
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
-  ];
-  
-  const Home: React.FC = () => {
-    const [collapsed, setCollapsed] = useState(false);
-  
-    return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
-          <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              Bill is a cat.
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout>
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Breadcrumb, Layout, Menu } from "antd";
+import React, { useState } from "react";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem("Option 1", "1", <PieChartOutlined />),
+  getItem("Option 2", "2", <DesktopOutlined />),
+  getItem("User", "sub1", <UserOutlined />, [
+    getItem("Tom", "3"),
+    getItem("Bill", "4"),
+    getItem("Alex", "5"),
+  ]),
+  getItem("Team", "sub2", <TeamOutlined />, [
+    getItem("Team 1", "6"),
+    getItem("Team 2", "8"),
+  ]),
+  getItem("Files", "9", <FileOutlined />),
+];
+
+const Home: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      {/* 左侧侧边栏 */}
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      {/* 右边内容 */}
+      <Layout className="site-layout">
+        {/* 右边头部 */}
+        <Header className="site-layout-background" style={{ paddingLeft: '16px' }}>
+          {/* 面包屑 */}
+          <Breadcrumb style={{ lineHeight: '64px' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+        </Header>
+        {/* 右边内容 */}
+        <Content className="site-layout-background" style={{ margin: "16px 16px 0" }}>
+            {/* 页面展示部分 根据路由变动 */}
+        </Content>
+        {/* 右边底部 */}
+        <Footer style={{ textAlign: "center", padding: 0, lineHeight: '48px' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
       </Layout>
-    );
-  };
-  
-  export default Home;
+    </Layout>
+  );
+};
+
+export default Home;

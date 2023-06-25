4d4c58eb7182a419fb8c97bfcd6afcf51d02e4b8
陈正清macbook pro <tenchenzhengqing@qq.com>
2023-06-25 19:04:05 +0800

    使用Antd中的侧边栏布局 静态页面完成

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,9 +2,9 @@
  * @Author: czqczqzzzzzz(czq)
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-06-23 16:19:13
- * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-06-24 20:32:18
- * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/App.tsx
+ * @LastEditors: 陈正清macbook pro
+ * @LastEditTime: 2023-06-25 18:01:17
+ * @FilePath: /react18-ts-vite_backend_management_system/vite-project/src/App.tsx
  * @Description: 所有组件的父组件App
  * 
  * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
@@ -13,15 +13,15 @@
 // import Comp2 from './components/Comp2'
 // import { Button } from 'antd'
 // import { FastBackwardOutlined } from '@ant-design/icons'
-import { Link, useRoutes } from 'react-router-dom'
+import { useRoutes } from 'react-router-dom'
 import router from '@/router'
 
 function App() {
   const outlet = useRoutes(router)
   return (
     <div className='App'>
-      <Link to='/home'>Home</Link>
-      <Link to='/about'>About</Link>
+      {/* <Link to='/home'>Home</Link>
+      <Link to='/about'>About</Link> */}
 
       {/* 占位符组件 类似于窗口，用来展示组件 有点像Vue中的router-view */}
       {/* <Outlet></Outlet> */}
--- vite-project/src/views/Home.tsx
+++ vite-project/src/views/Home.tsx
@@ -2,19 +2,78 @@
  * @Author: czqczqzzzzzz(czq)
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-06-24 18:15:11
- * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-06-24 19:19:23
- * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/views/Home.tsx
- * @Description: 路由写法 About组件
+ * @LastEditors: 陈正清macbook pro
+ * @LastEditTime: 2023-06-25 18:17:08
+ * @FilePath: /react18-ts-vite_backend_management_system/vite-project/src/views/Home.tsx
+ * @Description: Home首页组件
  * 
  * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
  */
-const View = () => {
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+  } from '@ant-design/icons';
+  import type { MenuProps } from 'antd';
+  import { Breadcrumb, Layout, Menu } from 'antd';
+  import React, { useState } from 'react';
+  
+  const { Header, Content, Footer, Sider } = Layout;
+  
+  type MenuItem = Required<MenuProps>['items'][number];
+  
+  function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+  ): MenuItem {
+    return {
+      key,
+      icon,
+      children,
+      label,
+    } as MenuItem;
+  }
+  
+  const items: MenuItem[] = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+      getItem('Tom', '3'),
+      getItem('Bill', '4'),
+      getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+  ];
+  
+  const Home: React.FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+  
     return (
-        <div className="home">
-            <p>这是Home组件</p>
-        </div>
-    )
-}
-
-export default View
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    );
+  };
+  
+  export default Home;

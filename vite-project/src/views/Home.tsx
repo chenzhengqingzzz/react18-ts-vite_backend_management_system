ffefb3b7b5909ec2c5e3341292f20e0931752090
陈正清macbook pro <tenchenzhengqing@qq.com>
2023-07-01 13:25:49 +0800

    为菜单栏绑定了点击事件并且知晓菜单栏组件传入的key参数可以作为存储路由path的容器

--- vite-project/src/views/Home.tsx
+++ vite-project/src/views/Home.tsx
@@ -3,7 +3,7 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-06-24 18:15:11
  * @LastEditors: 陈正清macbook pro
- * @LastEditTime: 2023-06-25 20:53:26
+ * @LastEditTime: 2023-07-01 13:18:52
  * @FilePath: /react18-ts-vite_backend_management_system/vite-project/src/views/Home.tsx
  * @Description: Home首页组件
  *
@@ -18,6 +18,7 @@ import {
 } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import { Breadcrumb, Layout, Menu } from "antd";
+import { log } from "console";
 import React, { useState } from "react";
 
 const { Header, Content, Footer, Sider } = Layout;
@@ -39,8 +40,8 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
+  getItem("Option 1", "/page1", <PieChartOutlined />),
+  getItem("Option 2", "/page2", <DesktopOutlined />),
   getItem("User", "sub1", <UserOutlined />, [
     getItem("Tom", "3"),
     getItem("Bill", "4"),
@@ -52,6 +53,18 @@ const items: MenuItem[] = [
   ]),
   getItem("Files", "9", <FileOutlined />),
 ];
+
+/**
+ * @description: 点击侧边菜单栏的点击事件回调
+ * @param {*} e 事件对象
+ * @return {*}
+ */
+const menuClick = (e: {key: string}) => {
+  console.log(e);
+  
+  // 点击跳转到对应的路由
+
+}
 
 const Home: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -70,24 +83,31 @@ const Home: React.FC = () => {
           defaultSelectedKeys={["1"]}
           mode="inline"
           items={items}
+          onClick={menuClick}
         />
       </Sider>
       {/* 右边内容 */}
       <Layout className="site-layout">
         {/* 右边头部 */}
-        <Header className="site-layout-background" style={{ paddingLeft: '16px' }}>
+        <Header
+          className="site-layout-background"
+          style={{ paddingLeft: "16px" }}
+        >
           {/* 面包屑 */}
-          <Breadcrumb style={{ lineHeight: '64px' }}>
+          <Breadcrumb style={{ lineHeight: "64px" }}>
             <Breadcrumb.Item>User</Breadcrumb.Item>
             <Breadcrumb.Item>Bill</Breadcrumb.Item>
           </Breadcrumb>
         </Header>
         {/* 右边内容 */}
-        <Content className="site-layout-background" style={{ margin: "16px 16px 0" }}>
-            {/* 页面展示部分 根据路由变动 */}
+        <Content
+          className="site-layout-background"
+          style={{ margin: "16px 16px 0" }}
+        >
+          {/* 页面展示部分 根据路由变动 */}
         </Content>
         {/* 右边底部 */}
-        <Footer style={{ textAlign: "center", padding: 0, lineHeight: '48px' }}>
+        <Footer style={{ textAlign: "center", padding: 0, lineHeight: "48px" }}>
           Ant Design ©2018 Created by Ant UED
         </Footer>
       </Layout>

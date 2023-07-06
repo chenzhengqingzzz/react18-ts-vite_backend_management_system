34046e646250c022d90202993368db2b916f5f79
陈正清MacPro <tenchenzhengqing@qq.com>
2023-07-06 18:54:13 +0800

    新增page401页面来测试

---
+++ vite-project/src/views/Page401.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-07-06 14:37:20
+ * @LastEditors: 陈正清MacPro
+ * @LastEditTime: 2023-07-06 18:19:17
+ * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/views/Page401.tsx
+ * @Description: page401组件
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
+ */
+const Page301 = () => {
+    return(
+        <div className="page401">
+            <p>这是Page401组件</p>
+        </div>
+    )
+}
+
+export default Page301

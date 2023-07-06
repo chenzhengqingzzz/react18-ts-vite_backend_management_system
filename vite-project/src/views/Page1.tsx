0978d3f3d16e7bdfd5f117e2e99690f2b15a8ffe
陈正清MacPro <tenchenzhengqing@qq.com>
2023-07-06 14:45:07 +0800

    添加组件说明

--- vite-project/src/views/Page1.tsx
+++ vite-project/src/views/Page1.tsx
@@ -1,3 +1,14 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-07-04 16:34:09
+ * @LastEditors: 陈正清MacPro
+ * @LastEditTime: 2023-07-06 14:38:43
+ * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/views/Page1.tsx
+ * @Description: Page1页面
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
+ */
 const Page1 = () => {
     return(
         <div className="page1">
--- vite-project/src/views/Page2.tsx
+++ vite-project/src/views/Page2.tsx
@@ -1,3 +1,14 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-07-04 16:34:09
+ * @LastEditors: 陈正清MacPro
+ * @LastEditTime: 2023-07-06 14:38:56
+ * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/views/Page2.tsx
+ * @Description: Page2页面
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
+ */
 const Page2 = () => {
     return(
         <div className="page2">

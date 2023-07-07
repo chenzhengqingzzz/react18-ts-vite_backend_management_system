31242e0fc96168fb40265c02fc067992fb646aae
陈正清MacPro <tenchenzhengqing@qq.com>
2023-07-07 15:09:02 +0800

    为登录页面配置路由规则

--- vite-project/src/router/index.tsx
+++ vite-project/src/router/index.tsx
@@ -3,7 +3,7 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-06-24 20:13:33
  * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-07-06 18:18:44
+ * @LastEditTime: 2023-07-07 14:23:14
  * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/router/index.tsx
  * @Description: 路由器
  * 
@@ -18,6 +18,7 @@ import Home from '@/views/Home';
 import { Navigate } from 'react-router-dom';
 
 // 定义懒加载模块
+const Login = lazy(() => import('@/views/Login'))
 const About = lazy(() => import('@/views/About'))
 const Page1 = lazy(() => import('@/views/Page1'))
 const Page2 = lazy(() => import('@/views/Page2'))
@@ -26,13 +27,13 @@ const Page401 = lazy(() => import('@/views/Page401'))
 
 /**
  * @description: 将懒加载的组件外层包裹一个React.Suspense标签 用来展示加载中的组件
- * @param {JSX.Element} lazyLoadComponentTagName 需要进行懒加载的组件
+ * @param {JSX.Element} lazyLoadComponent 需要进行懒加载的组件
  * @return {JSX.Element}
  */
-const withLazyLoadComponent = (lazyLoadComponentTagName: JSX.Element) => {
+const withLazyLoadComponent = (lazyLoadComponent: JSX.Element) => {
     return (
         <React.Suspense fallback={<div>Loading...</div>}>
-            {lazyLoadComponentTagName}
+            {lazyLoadComponent}
         </React.Suspense>
     )
 }
@@ -47,44 +48,47 @@ const routes = [
     {
         // 定义展示home路由及其子路由
         path: '/',
-        element: <Home/>,
+        element: <Home />,
         children: [
             {
                 path: '/page1',
-                element: withLazyLoadComponent(<Page1/>)
+                element: withLazyLoadComponent(<Page1 />)
             },
             {
                 path: '/page2',
-                element: withLazyLoadComponent(<Page2/>)
+                element: withLazyLoadComponent(<Page2 />)
             },
             {
                 path: '/page3/page301',
-                element: withLazyLoadComponent(<Page301/>)
+                element: withLazyLoadComponent(<Page301 />)
             },
             {
                 path: '/page4/page401',
-                element: withLazyLoadComponent(<Page401/>)
+                element: withLazyLoadComponent(<Page401 />)
             }
         ]
     },
     // 嵌套路由 结束------------------
 
+    {
+        path: '/login',
+        element: withLazyLoadComponent(<Login/>)
+    },
+    {
+        path: '/home',
+        element: <Home />
+    },
+    {
+        path: '/about',
+        // 使用懒加载的组件的写法，外面需要套一层类似Loading的标签来向用户提示加载组件
+        element: withLazyLoadComponent(<About />)
+    },
     // 配置路由表以外的所有路径重定向
     {
         path: '*',
         element: <Navigate to='/page1' /> // 使用重定向组件 重定向到Page1
 
     },
-    {
-        path: '/home',
-        element: <Home />
-    },
-
-    {
-        path: '/about',
-        // 使用懒加载的组件的写法，外面需要套一层类似Loading的标签来向用户提示加载组件
-        element: withLazyLoadComponent(<About/>)
-    }
 ]
 
 export default routes

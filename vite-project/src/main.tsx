440aeb6dd36fe963bf1f350d72d2e8f6c6b3d640
陈正清MacPro <tenchenzhengqing@qq.com>
2023-07-08 22:48:19 +0800

    解决登录后路由样式丢失的问题

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -3,7 +3,7 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-06-23 16:19:13
  * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-07-08 18:44:02
+ * @LastEditTime: 2023-07-08 20:47:53
  * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/main.tsx
  * @Description: 整个项目的入口文件
  *
@@ -14,6 +14,7 @@ import ReactDOM from "react-dom/client";
 // 全局引入reset-css 样式初始化一般放在最前
 import "reset-css";
 // UI框架的样式
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 // 全局样式
 import "@/assets/styles/global.scss";

0eb8adf4a4143655efb63a5166ff2dd57ee358b2
陈正清MacPro <tenchenzhengqing@qq.com>
2023-07-09 12:28:31 +0800

    将大仓库的state中的num抽离出来变成小仓库 实现了num的模块化

---
+++ vite-project/src/store/numStore/index.ts
@@ -0,0 +1,23 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-07-09 11:12:00
+ * @LastEditors: 陈正清MacPro
+ * @LastEditTime: 2023-07-09 11:57:59
+ * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/store/numStore/index.ts
+ * @Description: 属于num的小仓库
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
+ */
+
+export default {
+    state: {
+        num: 20
+    },
+    actions: {
+        // actions里面提交reducer需要处理的函数
+        increment(state: {num: number}, action: {type: string, data: number}){
+            state.num += action.data
+        }
+    }
+}
--- vite-project/src/store/reducer.ts
+++ vite-project/src/store/reducer.ts
@@ -1,6 +1,19 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-07-08 18:23:24
+ * @LastEditors: 陈正清MacPro
+ * @LastEditTime: 2023-07-09 12:26:37
+ * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/store/reducer.ts
+ * @Description: 加工状态的reducer 相当于Vue中的mutation react中的
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
+ */
+import numStore from "./numStore";
 // 初始化state
 const defautState = {
-    num: 20
+    // es6展开式
+    ...numStore.state
 }
 
 /**
@@ -10,11 +23,12 @@ const defautState = {
  * @return {Object}
  */
 let reducer = (state = defautState, action: {type: string, data: number}) => {
-    
+     
     // 组件中的dispatch一调用就会执行这里的代码
     switch (action.type) {
+        // 这里的条件的type是组件中的action规定的 和actions无关（actions是包含所有action的对象）
         case 'increment':
-            state.num += action.data
+            numStore.actions.increment(state, action)
             break;
     
         default:

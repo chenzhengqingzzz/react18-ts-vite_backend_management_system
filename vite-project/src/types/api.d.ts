6ca98257673ffc419ff342074521e30092d616dc
陈正清MacPro <tenchenzhengqing@qq.com>
2023-07-11 21:04:02 +0800

    类型书写错误 更改后提交

--- vite-project/src/types/api.d.ts
+++ vite-project/src/types/api.d.ts
@@ -3,7 +3,7 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-07-11 19:04:18
  * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-07-11 19:36:59
+ * @LastEditTime: 2023-07-11 21:03:46
  * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/types/api.d.ts
  * @Description: 专门定义所有请求/响应的参数类型
  * 
@@ -15,7 +15,7 @@
 // 验证码的响应参数类型约束
 declare interface CaptchaAPIRes {
     msg: string,
-    msg: string,
+    img: string,
     code: number,
     captchaEnabled: boolean,
     uuid: string

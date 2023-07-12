fec3c6f6083c78206be68ebed0f3ee98ec91cdcc
陈正清MacPro <tenchenzhengqing@qq.com>
2023-07-12 13:59:32 +0800

    做好登录请求函数和相关请求、响应参数的类型约束

--- vite-project/src/api/index.ts
+++ vite-project/src/api/index.ts
@@ -3,7 +3,7 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-07-11 16:33:59
  * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-07-11 19:34:36
+ * @LastEditTime: 2023-07-12 13:51:15
  * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/api/index.ts
  * @Description: 统一管理请求
  * 
@@ -22,5 +22,18 @@ export const reqGetCaptcha = (): Promise<CaptchaAPIRes> => {
     return requests({
         method: 'GET',
         url: '/prod-api/captchaImage'
+    })
+}
+
+/**
+ * @description: 登录请求 是get请求 有参数
+ * @param {LoginAPIReq} params 传递过来的用户登录信息
+ * @return {Promise<LoginAPIRes>}
+ */
+export const reqLogin = (params: LoginAPIReq): Promise<LoginAPIRes> => {
+    return requests({
+        method: 'GET',
+        url: '/prod-api/login',
+        data: params
     })
 }
--- vite-project/src/types/api.d.ts
+++ vite-project/src/types/api.d.ts
@@ -3,7 +3,7 @@
  * @Email: tenchenzhengqing@qq.com
  * @Date: 2023-07-11 19:04:18
  * @LastEditors: 陈正清MacPro
- * @LastEditTime: 2023-07-11 21:03:46
+ * @LastEditTime: 2023-07-12 13:50:56
  * @FilePath: /react18+ts+vite后台管理系统/vite-project/src/types/api.d.ts
  * @Description: 专门定义所有请求/响应的参数类型
  * 
@@ -19,4 +19,18 @@ declare interface CaptchaAPIRes {
     code: number,
     captchaEnabled: boolean,
     uuid: string
+}
+
+// 登录的请求参数类型约束
+declare interface LoginAPIReq {
+    username: string,
+    password: string,
+    code: string,
+    uuid: string
+}
+// 登录的响应参数类型约束
+declare interface LoginAPIRes {
+    msg: string,
+    code: number,
+    token: string
 }
